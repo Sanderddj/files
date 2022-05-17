e7f674721a2159e38c978eb72565156fbcf790c9
Sanderddj <104198194+Sanderddj@users.noreply.github.com>
2022-05-17 09:52:20 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ __winc_id__ = "ae539110d03e49ea8738fd413ac44ba8"
 __human_name__ = "files"
 
 import os.path
+import os
 from zipfile import ZipFile
 
 
@@ -11,9 +12,9 @@ def clean_cache():
     if os.path.isdir('files\cache'):
         for file in os.scandir(path+'\cache'):
             os.remove(file.path)
-        print('aanwezig')
+        #print('aanwezig')
     else:
-        print('niet aanwezig')
+        #print('niet aanwezig')
         os.mkdir('files\cache')
     return
 
@@ -21,13 +22,13 @@ def cache_zip(path_zip,path_cache1):
     clean_cache()
     with ZipFile(path_zip, 'r') as zipObj:
         zipObj.extractall(path_cache1)
-    print('File is unzipped in cache folder') 
+    #print('File is unzipped in cache folder') 
 
-a = 'C:\\Users\\Sddej\\Documents\\03 opleiding\\winc\\files\\data.zip'
-b = 'C:\\Users\\Sddej\\Documents\\03 opleiding\\winc\\files\\cache'
+a = os.getcwd()+'\\files\\data.zip'
+b = os.getcwd()+'\\files\\cache'
 #cache_zip(a,b)
 def cached_files():
-    mypath = 'C:\\Users\\Sddej\\Documents\\03 opleiding\\winc\\files\\cache'
+    mypath = os.getcwd()+'\\files\\cache'
     path = os.path.abspath(mypath)
     list_dirs = [entry.path for entry in os.scandir(path) if entry.is_file()]
     #print(list_dirs)
